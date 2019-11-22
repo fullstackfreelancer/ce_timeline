71403ea6ca2ea1492d510bbf26b3d7777036f26c
Simon Köhler <info@simon-koehler.com>
2019-11-22 12:13:49 -0500

    Version number changed

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array (
     'constraints' =>
     array (
         'depends' => array (
-            'typo3' => '8.7.0-10.5.99'
+            'typo3' => '8.7.0-10.1.99'
         ),
         'conflicts' => array (
 

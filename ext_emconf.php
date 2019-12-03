54ac534e9e1ce85b29590fd11c6f816be3bebc0d
Simon Köhler <info@simon-koehler.com>
2019-12-03 18:51:58 -0500

    Updated TYPO3 version number to 10.2.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array (
     'constraints' =>
     array (
         'depends' => array (
-            'typo3' => '8.7.0-10.1.99'
+            'typo3' => '8.7.0-10.2.99'
         ),
         'conflicts' => array (
 

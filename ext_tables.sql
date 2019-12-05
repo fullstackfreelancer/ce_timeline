95fcb5dfa806f0d76e472fd48ee978602e8cc044
Simon Köhler <info@simon-koehler.com>
2019-12-05 10:59:01 -0500

    Removed unused database tables for versioning

--- ext_tables.sql
+++ ext_tables.sql
@@ -23,16 +23,6 @@ CREATE TABLE tx_cetimeline_domain_model_entry (
     sorting int(11) unsigned DEFAULT '0' NOT NULL,
     categories int(11) unsigned DEFAULT '0' NOT NULL,
 
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
 	l10n_parent int(11) DEFAULT '0' NOT NULL,
 	l10n_diffsource mediumblob,

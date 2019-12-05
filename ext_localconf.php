ba9ecf49fc429e23232a58b704ff5b6be9120ac5
Simon Köhler <info@simon-koehler.com>
2019-12-05 12:15:37 -0500

    Clean up

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
    \TYPO3\CMS\Core\Imaging\IconRegistry::class
 );
 $iconRegistry->registerIcon(
-   'timeline-icon', // Icon-Identifier, z.B. tx-myext-action-preview
+   'timeline-icon', // Icon-Identifier
    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
    ['source' => 'EXT:ce_timeline/Resources/Public/Icons/content-timeline.svg']
 );

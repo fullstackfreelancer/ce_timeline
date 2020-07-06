06434b3b7fb00f5be9dc199377084b93a3620e29
Simon Köhler <info@simon-koehler.com>
2020-07-05 20:27:01 -0500

    Setting default values

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -5,3 +5,5 @@
     'Configuration/TypoScript/TsConfig/page.typoscript',
     'Timeline Content Element'
 );
+
+$GLOBALS['TCA']['tx_cetimeline_domain_model_entry']['columns']['content_elements']['config']['overrideChildTca']['columns']['colPos']['config']['default'] = '1978';

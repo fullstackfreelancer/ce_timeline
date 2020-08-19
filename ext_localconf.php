8fa4763ae851fe9322288aa650a985178ab97e3b
Simon Köhler <info@simon-koehler.com>
2020-08-19 10:23:31 -0500

    icon renaming

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,8 @@ call_user_func(
     function()
     {
         $icons = [
-            'timeline-icon' => 'EXT:ce_timeline/Resources/Public/Icons/content-timeline.svg',
+            'ce-timeline-icon' => 'EXT:ce_timeline/Resources/Public/Icons/content-timeline.svg',
+            'ce-timeline-record-icon' => 'EXT:ce_timeline/Resources/Public/Icons/content-timeline-record.svg',
         ];
         $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
         foreach ($icons as $identifier => $path) {

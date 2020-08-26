fcd0e3da07675c661a0f3b436b22ae566824f68b
Simon Köhler <info@simon-koehler.com>
2020-08-26 08:33:16 -0500

    cleanup

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,12 +2,13 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
-    function()
-    {
+    function() {
+
         $icons = [
             'ce-timeline-icon' => 'EXT:ce_timeline/Resources/Public/Icons/content-timeline.svg',
             'ce-timeline-record-icon' => 'EXT:ce_timeline/Resources/Public/Icons/content-timeline-record.svg',
         ];
+
         $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
         foreach ($icons as $identifier => $path) {
             $iconRegistry->registerIcon(

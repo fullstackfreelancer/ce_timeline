bc5c912dd38e05219945a81dba4894620921414d
fullstackfreelancer <simonkoehler1978@gmail.com>
2023-11-13 17:14:01 -0500

    Remove deprecated function

--- ext_tables.php
+++ ext_tables.php
@@ -5,7 +5,7 @@ call_user_func(
     function() {
 
         // Enable TYPO3 to create records on standard pages instead of only sys folders
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_cetimeline_domain_model_entry');
+        //\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_cetimeline_domain_model_entry');
 
         // Hide table in all list views, in order to provide smooth inline editing experience
         $TCA['tx_cetimeline_domain_model_entry']['ctrl']['hideTable'] = 1;

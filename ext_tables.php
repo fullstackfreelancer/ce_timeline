9119c554fbb98119bac47ee1a1e63ade1ff4455d
fullstackfreelancer <simonkoehler1978@gmail.com>
2023-11-14 07:38:22 -0500

    Cleaned up old code

--- ext_tables.php
+++ ext_tables.php
@@ -4,9 +4,6 @@ defined('TYPO3') || die('Access denied.');
 call_user_func(
     function() {
 
-        // Enable TYPO3 to create records on standard pages instead of only sys folders
-        //\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_cetimeline_domain_model_entry');
-
         // Hide table in all list views, in order to provide smooth inline editing experience
         $TCA['tx_cetimeline_domain_model_entry']['ctrl']['hideTable'] = 1;
 

22aaa401a64ffa3eb07c48067653a0916c4e9149
fullstackfreelancer <simonkoehler1978@gmail.com>
2023-11-14 05:12:33 -0500

    Change from TYPO3_MODE to TYPO3 for compatibility with v12

--- Configuration/TCA/Overrides/tt_content_ce_timeline.php
+++ Configuration/TCA/Overrides/tt_content_ce_timeline.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || die();
 
 call_user_func(function () {
 	$frontendLanguageFilePrefix = 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:';

fb5583b3fe6ebf05040eefc38994dbc73c31567f
fullstackfreelancer <simonkoehler1978@gmail.com>
2023-11-14 05:39:31 -0500

    Update versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,11 +9,11 @@ $EM_CONF['ce_timeline'] = array (
     'state' => 'stable',
     'uploadfolder' => '0',
     'clearCacheOnLoad' => 0,
-    'version' => '3.3.3',
+    'version' => '4.0.0',
     'constraints' =>
     array (
         'depends' => array (
-            'typo3' => '9.5.0-11.5.99'
+            'typo3' => '11.8.99-12.4.7'
         ),
         'conflicts' => array (
 
